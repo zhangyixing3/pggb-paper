446153179609697b2f7f8697778ae1dfafc190cd
Zhang Yixing <zyxjh21@126.com>
2025-03-23 00:51:28 +0800

    Update vcf_preprocess.sh

--- scripts/vcf_preprocess.sh
+++ scripts/vcf_preprocess.sh
@@ -2,12 +2,13 @@
 
 # From https://github.com/wwliao/pangenome-utils/blob/main/preprocess_vcf.sh
 # Usage: preprocess_vcf.sh <VCF file> <sample name> <max variant size>
+# example: vcf_process.sh 97samples.d10.vcf.gz 097_yilong 100000 Col-CEN_v1.2.fastaa
+
 
 VCF=$1
 SAMPLE=$2
-REF=$3
-CHROMS=$4
-MAXSIZE=$5
+MAXSIZE=$3
+REF=$4
 
 PREFIX=$(basename $VCF .vcf.gz)
 
@@ -20,7 +21,7 @@ bcftools view -a -s ${SAMPLE} -Ou ${VCF} \
     | bcftools norm -d exact -Oz --threads 48 -o ${PREFIX}.norm.vcf.gz \
     && bcftools index -t ${PREFIX}.norm.vcf.gz \
     && bcftools view -e "STRLEN(REF)>${MAXSIZE} | STRLEN(ALT)>${MAXSIZE}" \
-                 -r ${nCHROMS} --threads 48 -Oz -o ${PREFIX}.norm.max${MAXSIZE}.vcf.gz \
+                 --threads 48 -Oz -o ${SAMPLE}.norm.max${MAXSIZE}.vcf.gz \
                  ${PREFIX}.norm.vcf.gz \
-    && bcftools index -t ${PREFIX}.norm.max${MAXSIZE}.vcf.gz \
+    && bcftools index -t ${SAMPLE}.norm.max${MAXSIZE}.vcf.gz  -f \
     && rm ${PREFIX}.norm.vcf.gz*
